9af5bacbeef6e5e4260d227d79e4dff78292cd1a
Luis Adame <mrluissan@gmail.com>
2018-11-27 12:57:03 +0100

    Add styles and lightbox

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,13 @@
 import Turbolinks from 'turbolinks';
+import {
+    LuminousGallery
+} from 'luminous-lightbox';
 Turbolinks.start();
+document.addEventListener('turbolinks:click', function (event) {
+    if (event.target.getAttribute('href').charAt(0) === '#') {
+        return event.preventDefault()
+    }
+});
+new LuminousGallery(document.querySelectorAll('img'), {}, {
+    sourceAttribute: 'src'
+});
